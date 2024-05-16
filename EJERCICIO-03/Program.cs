6c146cb4005c6cb93d0bb23785e978c8288074bf
J0N4T4N1508 <167550570+J0N4T4N1508@users.noreply.github.com>
2024-05-16 10:37:25 -0500

    Add files via upload

---
+++ EJERCICIO-03/Program.cs
@@ -0,0 +1,16 @@
+﻿// Equivalencia de numeros a romanos
+
+List<string> romanos = new List<string>() {"I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIV","XX"};
+
+int numero;
+Console.WriteLine("Ingrese un numero del 1 al 20: ");
+numero = int.Parse(Console.ReadLine());
+
+if (numero >= 1 && numero <= 20)
+{
+    Console.WriteLine($"El equivalente del numero {numero} en romanoes es: {romanos[numero - 1]}");
+}
+else {
+    Console.WriteLine("Numero fuera de rango");
+}
+
