63e45d8324ef0ab54e17afb22887f6a1f2381b2b
J0N4T4N1508 <167550570+J0N4T4N1508@users.noreply.github.com>
2024-05-16 10:38:05 -0500

    Add files via upload

---
+++ EJERCICIO-04/Program.cs
@@ -0,0 +1,30 @@
+﻿// Registrar una matriz, ordenarla, mostrar el original y la ordenada.
+
+using System.Runtime.InteropServices;
+
+int[] arreglo = new int[5];
+
+arreglo[0] = 4;
+arreglo[1] = 3;
+arreglo[2] = 7;
+arreglo[3] = 1;
+arreglo[4] = 9;
+
+Console.WriteLine("Matriz original: ");
+
+for (int i = 0; i < arreglo.Length; i++) {
+    Console.Write($"[{arreglo[i]}]");
+}
+
+List<int> listaTemporal = arreglo.ToList();
+listaTemporal.Sort();
+
+Console.WriteLine(" ");
+Console.WriteLine("Matriz ordenada:");
+
+
+int [] arregloOrdenado= listaTemporal.ToArray();
+
+for (int i = 0;i < arregloOrdenado.Length;i++) {
+    Console.Write($"[{arregloOrdenado[i]}]");
+}
